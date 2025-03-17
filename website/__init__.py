42f641ee365ab8f61fad00d314d107d7d177a018
JanHansenEW <jan.hansen@everyware.ch>
2025-03-17 10:03:26 +0100

    new file:   .vscode/launch.json 	new file:   instance/database.db 	modified:   website/__init__.py 	new file:   website/__pycache__/__init__.cpython-313.pyc 	new file:   website/__pycache__/auth.cpython-313.pyc 	new file:   website/__pycache__/models.cpython-313.pyc 	new file:   website/__pycache__/views.cpython-313.pyc 	modified:   website/views.py

--- website/__init__.py
+++ website/__init__.py
@@ -4,13 +4,13 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "todo.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqldb://JanHansen:Welcome2024$@JanHansen.mysql.pythonanywhere-services.com:3306/todo'
     db.init_app(app)
 
     from .views import views
@@ -35,7 +35,7 @@ def create_app():
     return app
 
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+#def create_database(app):
+#    if not path.exists('website/' + DB_NAME):
+        # db.create_all(app=app)
+        # print('Created Database!')
--- website/views.py
+++ website/views.py
@@ -11,13 +11,13 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+        note = request.form.get('note') #Gets the note from the HTML 
 
         if len(note) < 1:
             flash('Note is too short!', category='error') 
         else:
             new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+            db.session.add(new_note)  #adding the note to the database 
             db.session.commit()
             flash('Note added!', category='success')
 
